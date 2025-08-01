a649b11b1d4999350ffe8d369fa16c5f8c43c6c5
Darius Cimbru <59187994+Diamonddragon999@users.noreply.github.com>
2025-08-01 22:09:12 +0300

    Update rarityWeights.ts

--- src/utils/rarityWeights.ts
+++ src/utils/rarityWeights.ts
@@ -1,9 +1,9 @@
     // src/utils/rarityWeights.ts
 /* src/utils/rarityWeights.ts */
 export const RARITY_WEIGHTS = {
-  voucher   : 34,   // “comun” – ∞
+  voucher   : 14,   // “comun” – ∞
   rare      : 30,
-  epic      : 30,
-  legendary : 6,
+  epic      : 40,
+  legendary : 16,
 } as const;
 
